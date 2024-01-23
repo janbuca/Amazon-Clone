5b38abeebeeeee4dfd654b8b57301a35bb6f1342
janbuca <janbuca333@gmail.com>
2024-01-23 00:52:38 -0800

    The working the Checkout Folder under the checkout checkout.js and checkout.css can be done

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,19 +1,44 @@
 import './App.css';
+import Checkout from './Componets/Checkout/Checkout';
 import Header from './Componets/Header/Header';
 import Home from './Componets/Home/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 
 
 
 function App() {
   return (
+
+    <Router>
     <div className="App">
-      <Header/>
-      <Home/>
+      <Routes>
+      
+        {/* <Route path="Login" element = {<Login />}/>  */}
+        <Route path="/" element = {<> <Header /> <Home /> </>}/>
+        <Route path="/Checkout" element = {<> <Header /> <Checkout /> </>}/>
+        {/* <Route  path="/Payment" element = {<> <Header/>
+          <Elements stripe={promise}>
+            <Payment />
+          </Elements> </>} />
+        <Route  path="/orders" element = {<> <Header/> <Orders /> </>} /> */}
+
+      </Routes>				
+    </div>
+  </Router>
+
+
+
+
+    // <div >
+      // <Header/>
+      // <Home/>
+      
 
      
       
-    </div>
+    // </div>
   );
 }
 
